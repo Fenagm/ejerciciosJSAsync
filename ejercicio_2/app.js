1ca42198bcc9095d5f7c2eb9ccb25a483876e96f
Fernando <fernandogm@live.com.ar>
2020-09-21 13:44:11 -0300

    terminado

--- ejercicio_2/app.js
+++ ejercicio_2/app.js
@@ -1,5 +1,7 @@
 
 const obtenerChiste = require("./library");
 
+obtenerChiste().then(e => console.log( `${e[0].setup} ${e[0].punchline}`)) ;
+
 // Usar la funcion obtenerChiste() la cual devuelve la promesa de traer el objeto chiste extraido
 obtenerChiste();
--- ejercicio_3/app.js
+++ ejercicio_3/app.js
@@ -1,14 +1,12 @@
 
 const obtenerPokemon = require("./library");
 
-// Codigo funcion callback
+let pokemon = (element) => { 
+    let abilites = element.abilities.map(abilitie => {
+        return abilitie.ability.name
+    });
+    console.log(`Nombre: ${element.name} `) 
+    console.log(`Habilidades: ${abilites}  `) 
+}
 
-
-
-// Fin codigo
-
-// Usar la funcion obtenerPokemon(pokemon,funcionCallback) en donde:
-// - nombrePokemon es un string con el nombre del pokemon
-// - funcionCallback es una función que recibe el objeto con los datos del pokemon extraido
-
-obtenerPokemon();
+obtenerPokemon('pikachu', pokemon);
